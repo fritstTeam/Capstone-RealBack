bb00ad0cffdcc3c4a81c8a7ed8833d23705619c2
JYE <jo1814@naver.com>
2025-04-01 21:30:06 +0900

    first commit

---
+++ src/main/java/idusw/sbb/maple/MapleApplication.java
@@ -0,0 +1,13 @@
+package idusw.sbb.maple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MapleApplication.class, args);
+  }
+
+}
