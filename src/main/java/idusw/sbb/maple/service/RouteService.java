234f59368f406174f7ba554011d10875a45d73dd
JYE <jo1814@naver.com>
2025-04-29 14:44:11 +0900

    Route Service 생성 및, Create 기능 구현

---
+++ src/main/java/idusw/sbb/maple/service/RouteService.java
@@ -0,0 +1,37 @@
+package idusw.sbb.maple.service;
+
+import idusw.sbb.maple.controller.dto.route.CreateRouteRequest;
+import idusw.sbb.maple.domain.Category;
+import idusw.sbb.maple.domain.Route;
+import idusw.sbb.maple.domain.User;
+import idusw.sbb.maple.mapper.RouteMapper;
+import idusw.sbb.maple.repository.CategoryRepository;
+import idusw.sbb.maple.repository.RouteRepository;
+import idusw.sbb.maple.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RouteService {
+
+  private final RouteRepository routeRepository;
+  private final UserRepository userRepository;
+  private final CategoryRepository categoryRepository;
+
+  public RouteService(RouteRepository routeRepository, UserRepository userRepository,
+      CategoryRepository categoryRepository) {
+    this.routeRepository = routeRepository;
+    this.userRepository = userRepository;
+    this.categoryRepository = categoryRepository;
+  }
+
+  public Route createRoute(Long userIdx, CreateRouteRequest route) {
+    User user = userRepository.findById(userIdx)
+        .orElseThrow(() -> new RuntimeException("User not found"));
+
+    Category category = categoryRepository.findById(route.getCategoryIdx())
+        .orElseThrow(() -> new RuntimeException("Category not found"));
+
+    return routeRepository.save(RouteMapper.toPersistence(user, category, route));
+  }
+
+}
