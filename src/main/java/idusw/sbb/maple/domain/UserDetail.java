2f96d2187d299ae571eafb12e7a169e5fb38405e
JYE <jo1814@naver.com>
2025-05-15 19:58:23 +0900

    UserDetail 객체 수정

--- src/main/java/idusw/sbb/maple/domain/UserDetail.java
+++ src/main/java/idusw/sbb/maple/domain/UserDetail.java
@@ -1,52 +1,38 @@
 package idusw.sbb.maple.domain;
 
+import java.util.Collection;
+import java.util.List;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 @Getter
 public class UserDetail implements UserDetails {
 
-    private Long userIdx;
-    private String username;
-    private String password;
-    private String nickname;
-    private LocalDateTime createdAt;
-    private List<Route> routes = new ArrayList<>();
-    private List<Board> boards = new ArrayList<>();
-    private List<Comment> comments = new ArrayList<>();
+  public UserDetail(Long userIdx, String userId, String userNickname, String userPassword) {
+    this.userIdx = userIdx;
+    this.userId = userId;
+    this.userNickname = userNickname;
+    this.userPassword = userPassword;
+  }
 
-    public UserDetail(User user) {
-        this.userIdx = user.getUserIdx();
-        this.username = user.getUserId();
-        this.password = user.getUserPassword();
-        this.nickname = user.getNickname();
-        this.createdAt = user.getCreatedAt();
-        this.routes = user.getRoutes();
-        this.boards = user.getBoards();
-        this.comments = user.getComments();
+  private Long userIdx;
+  private String userId;
+  private String userNickname;
+  private String userPassword;
 
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
+  @Override
+  public String getPassword() {
+    return userPassword;
+  }
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-
+  @Override
+  public String getUsername() {
+    return userId;
+  }
 }
