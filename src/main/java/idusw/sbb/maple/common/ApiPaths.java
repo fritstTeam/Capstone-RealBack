84277f55a3019e3593bbbd499a59c8cc41ab94f4
JYE <jo1814@naver.com>
2025-05-15 19:56:16 +0900

    ApiPaths 상수 추가

--- src/main/java/idusw/sbb/maple/common/ApiPaths.java
+++ src/main/java/idusw/sbb/maple/common/ApiPaths.java
@@ -4,7 +4,12 @@ public class ApiPaths {
 
   public static final String API_V1 = "/api/v1";
 
+  public static final String LOGIN = "/login";
+  public static final String LOGOUT = "/logout";
+
+
   public static final String ROUTES = "/routes";
   public static final String CATEGORIES = "/categories";
+  public static final String USERS = "/users";
 
 }
