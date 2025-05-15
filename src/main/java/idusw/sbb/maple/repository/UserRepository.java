f8a3f738ec091452978b485cece5790075e0a043
JYE <jo1814@naver.com>
2025-05-15 19:58:03 +0900

    User Repo (I) 수정

--- src/main/java/idusw/sbb/maple/repository/UserRepository.java
+++ src/main/java/idusw/sbb/maple/repository/UserRepository.java
@@ -1,19 +1,14 @@
 package idusw.sbb.maple.repository;
 
 import idusw.sbb.maple.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-  User findByUserId(String userId);
+  Optional<User> findByUserId(String userId);
 
   boolean existsByUserId(String userId);
-
-  @Override
-  <S extends User> S save(S entity); // insert or update
-
-  @Override
-  void deleteById(Long userId);
 }
