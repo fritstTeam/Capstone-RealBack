3500ff8bc89655b861457cae78a3620f50873a0a
JYE <jo1814@naver.com>
2025-05-15 20:10:15 +0900

    SequrityConfig 관련 유저 인증을 위한 authenticationManager 메서드 구현 및, 인증 관련 로직 Controller로 옮김에 따른 기존 로직 주석화 (직관적이고 문서화의 편의를 위해 주석처리)

--- src/main/java/idusw/sbb/maple/config/SecurityConfig.java
+++ src/main/java/idusw/sbb/maple/config/SecurityConfig.java
@@ -1,19 +1,15 @@
 package idusw.sbb.maple.config;
 
 
-import idusw.sbb.maple.domain.UserDetail;
-import idusw.sbb.maple.service.JwtAuthenticationFilter;
-import idusw.sbb.maple.service.JwtService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -22,41 +18,49 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(csrf-> csrf.disable()) // CSRF 끄기 (POST 요청 테스트 편하게)
-        .formLogin(formLogin -> formLogin
-                .loginProcessingUrl("/login")
-                .usernameParameter("id")
-                .passwordParameter("pwd")
-                .successHandler((request, response, authentication) -> {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.setContentType("text/plain;charset=UTF-8");
-                    response.getWriter().write("로그인 성공");
-                })
-                .failureHandler((request, response, exception) -> {
-                  response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                  response.getWriter().write("로그인 실패");
-                }))
-            .logout(logout -> logout
-                    .logoutUrl("/logout")                        // 로그아웃 요청 URL
-                    .logoutSuccessHandler((request, response, authentication) -> {
-                        response.setStatus(HttpServletResponse.SC_OK);
-                        response.setContentType("text/plain;charset=UTF-8");
-                        response.getWriter().write("로그아웃 성공");
-                    })
-                    .invalidateHttpSession(true)                 // 세션 무효화
-                    .deleteCookies("JSESSIONID")                 // 세션 쿠키 삭제
-            )
-        .httpBasic(httpBasic -> httpBasic.disable()) // 브라우저 팝업 로그인 끔
+        .csrf(AbstractHttpConfigurer::disable) // CSRF 끄기 (POST 요청 테스트 편하게)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .logout(AbstractHttpConfigurer::disable)
+        /* .formLogin(formLogin -> formLogin
+             .loginProcessingUrl("/login")
+             .usernameParameter("id")
+             .passwordParameter("pwd")
+             .successHandler((request, response, authentication) -> {
+               response.setStatus(HttpServletResponse.SC_OK);
+               response.setContentType("text/plain;charset=UTF-8");
+               response.getWriter().write("로그인 성공");
+             })
+             .failureHandler((request, response, exception) -> {
+               response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+               response.getWriter().write("로그인 실패");
+             }))
+         .logout(logout -> logout
+             .logoutUrl("/logout")                        // 로그아웃 요청 URL
+             .logoutSuccessHandler((request, response, authentication) -> {
+               response.setStatus(HttpServletResponse.SC_OK);
+               response.setContentType("text/plain;charset=UTF-8");
+               response.getWriter().write("로그아웃 성공");
+             })
+             .invalidateHttpSession(true)                 // 세션 무효화
+             .deleteCookies("JSESSIONID")                 // 세션 쿠키 삭제
+         )*/
+        .httpBasic(AbstractHttpConfigurer::disable) // 브라우저 팝업 로그인 끔
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()  // 모든 요청 허용
-        ).cors(c-> c.configurationSource(new CorsConfig()));
+        ).cors(c -> c.configurationSource(new CorsConfig()));
 
     return http.build();
   }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
 }
