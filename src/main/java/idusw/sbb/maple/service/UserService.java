3da3a919d95c2f85332e1ea9e8de35c820a0d445
JYE <jo1814@naver.com>
2025-05-15 20:08:20 +0900

    UserService 관련 안쓰는 메서드 삭제 및 흐름 최적화

--- src/main/java/idusw/sbb/maple/service/UserService.java
+++ src/main/java/idusw/sbb/maple/service/UserService.java
@@ -1,75 +1,51 @@
 package idusw.sbb.maple.service;
 
+import idusw.sbb.maple.controller.dto.user.UserRequest;
 import idusw.sbb.maple.domain.User;
 import idusw.sbb.maple.domain.UserDetail;
+import idusw.sbb.maple.exception.HttpException;
+import idusw.sbb.maple.mapper.UserMapper;
 import idusw.sbb.maple.repository.UserRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Map;
-
 
 @Service
 public class UserService implements UserDetailsService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+    User user = userRepository.findByUserId(id)
+        .orElseThrow(() -> new HttpException("Id Or Password is Wrong", HttpStatus.NOT_FOUND));
+
+    return new UserDetail(user.getUserIdx(), user.getUserId(), user.getNickname(),
+        user.getUserPassword());
+  }
+
+  public void createUser(UserRequest user) {
+    existByUserId(user.getUserId());
+
+    String encodedPassword = passwordEncoder.encode(user.getUserPassword());
+    User persistence = UserMapper.toPersistence(user, encodedPassword);
+
+    userRepository.save(persistence);
+  }
+
+  public void existByUserId(String id) {
+    if (userRepository.existsByUserId(id)) {
+      throw new HttpException("Id Already Exists", HttpStatus.CONFLICT);
     }
-
-    @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        User user = userRepository.findByUserId(id);
-
-        if(user == null) {
-            throw new UsernameNotFoundException(id);
-        }
-
-        return new UserDetail(user);
-    }
-
-    public <T> void userSave(Map userData){ // 회원가입 or 회원수정
-        User user = new User();
-        user.setUserId(userData.get("userId").toString());
-        user.setUserPassword(passwordEncoder.encode(userData.get("password").toString()));
-        user.setNickname(userData.get("name").toString());
-
-        System.out.println("서비스user"+user);
-
-        if (userRepository.existsByUserId(userData.get("userId").toString())) {
-            throw new IllegalArgumentException("가입 정보를 확인해주세요.");
-        }
-
-        userRepository.save(user);
-    }
-
-    public boolean existByUserId(String userId){
-        return userRepository.existsByUserId(userId);
-    }
-
-    public Long getUserIdx(Authentication authentication) {
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return null;
-        } else{
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof UserDetail userDetail) {
-                return userDetail.getUserIdx(); //사용자 객체
-            } else {
-                return null;
-            }
-        }
-
-    }
-
+  }
 }
